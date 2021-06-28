f075d2e85e5062344ece91b5e52b3ffb4a6223c9
Barbara Perez <bere@getnea.com>
2021-06-28 16:50:52 -0500

    Se solicita autenticacion para ver y crear de las vistas: Peliculas, Detalles y Director. La vista de Musica es solo de lectura y se requiere autentiacion para crear

--- movies/peliculas/views.py
+++ movies/peliculas/views.py
@@ -1,3 +1,4 @@
+from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
 
 from peliculas.models import Pelicula, Director, Detalles
@@ -6,11 +7,14 @@ from peliculas.serializers import PeliculaSerializer, DirectorSerializer, Detall
 class PeliculaViewSet(ModelViewSet):
     queryset = Pelicula.objects.all().select_related('director_pelicula')
     serializer_class = PeliculaSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class DirectorViewSet(ModelViewSet):
     queryset = Director.objects.all()
     serializer_class = DirectorSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class DetallesViewSet(ModelViewSet):
     queryset = Detalles.objects.all().select_related('nombre', 'musica')
     serializer_class = DetallesSerializer
+    permission_classes = [permissions.IsAuthenticated]
