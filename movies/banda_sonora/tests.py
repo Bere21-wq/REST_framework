80212ec7444f9ea89125a877138efa60a6235b4f
Barbara Perez <bere@getnea.com>
2021-07-01 14:12:07 -0500

    Se corrigen las pruebas para GET y POST y se agregan pruebas de PUT y DELETE. Resta modificar el nombre de las variables para que sean más claras de entender.

--- movies/banda_sonora/tests.py
+++ movies/banda_sonora/tests.py
@@ -23,8 +23,8 @@ class TestMusicaView(APITestCase):
         """
         respuesta = self.client.get(self.response)
         self.assertEqual(200, respuesta.status_code)
-        print(self.response)
-        print(respuesta.data)
+        # print(self.response)
+        # print(respuesta.data)
         
     def test_numero_elementos_BD(self):
         """
@@ -35,30 +35,34 @@ class TestMusicaView(APITestCase):
         num_elementos     = Musica.objects.count()
         self.assertEqual(num_elementos, len(respuesta.data))
 
-    # def test_match_elementos_BD(self):
-    #     """
-    #     Prueba que cada elemento de la lista coincide con 
-    #     un elemento en la base de datos.
-    #     """
-    #     respuesta = self.client.get(self.response)
-    #     print(respuesta.data)
-    #     objeto_1 = Musica.objects.filter(cancion__contains=respuesta.data[0]['cancion'], artista__contains=respuesta.data[0]['artista'])
-    #     objeto_2 = Musica.objects.filter(cancion__contains=respuesta.data[1]['cancion'], artista__contains=respuesta.data[1]['artista'])
-        
-    #     if objeto_1.exists and objeto_2.exists:
-    #         print('Los registros estan en la base de datos')
-    #     else:
-    #         pass
+    def test_match_elementos_BD(self):
+        """
+        Prueba que cada elemento de la lista coincide con 
+        un elemento en la base de datos.
+        """
+        respuesta = self.client.get(self.response)
+
+        for i in respuesta.data:
+            #print(i)
+            v = Musica.objects.get(id=i['id'])
+            self.assertEqual(i['artista'], v.artista)
+            self.assertEqual(i['owner'], v.owner.username)
+            #print(Musica.objects.filter(id=i['id']).count())
 
     def test_recupera_un_solo_elemento(self):
          """
          Prueba si se recupera solo uno de los registros de la lista
          """
-         self.detalle = reverse('musica:detail', kwargs={'pk' : self.musica1.id})
-         respuesta = self.client.get(self.detalle)
+         detalle = reverse('musica:detail', kwargs={'pk' : self.musica1.id})
+         respuesta = self.client.get(detalle)
+         #print(respuesta.data['id'])
          self.assertEqual(200, respuesta.status_code)
-         self.assertEqual(respuesta.data, {'id': 1, 'owner': 'testuser1', 'cancion': 'cancion test 1', 'artista': 'artista test 1', 'año': '2021-06-16', 'genero': 'Metal'})
- 
+         valor_consulta = Musica.objects.get(id=respuesta.data['id'])
+         #print(valor_consulta)
+         self.assertEqual(respuesta.data['cancion'], valor_consulta.cancion)
+         self.assertEqual(respuesta.data['artista'], valor_consulta.artista)
+         self.assertEqual(respuesta.data['año'], str(valor_consulta.año))
+
     def test_crear_POST_con_autenticacion(self):
         """
         Prueba si un usuario loggeado puede crear un registro de banda sonora.
@@ -66,7 +70,7 @@ class TestMusicaView(APITestCase):
         self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
         data_info = {'cancion': 'cancion test', 'artista':'artista test', 'año':'2021-06-16', 'genero':'Metal'}
         respuesta = self.client.post(self.response, data_info)
-        print(respuesta.data)      
+        #print(respuesta.data)      
         self.assertEqual(201, respuesta.status_code)
         
     def test_crear_POST_sin_autenticacion(self):
@@ -75,7 +79,7 @@ class TestMusicaView(APITestCase):
         """
         data_info = {'cancion': 'cancion test', 'artista':'artista test', 'año':'2021-06-16', 'genero':'Metal'}
         respuesta = self.client.post(self.response, data_info)
-        print(respuesta.data)      
+        #print(respuesta.data)      
         self.assertEqual(401, respuesta.status_code)
     
 
@@ -85,11 +89,11 @@ class TestMusicaView(APITestCase):
         a 'n+1' después de crear un registro.
         """
         self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
-        respuesta_pre_post     = self.client.get(self.response)
+        respuesta_pre_post     = Musica.objects.count()
         data_info              = {'cancion': 'cancion test', 'artista':'artista test', 'año':'2021-06-16', 'genero':'Metal'}
         respuesta_post         = self.client.post(self.response, data_info)
         num_elementos_pos_post = Musica.objects.count()
-        self.assertEqual(num_elementos_pos_post, len(respuesta_pre_post.data)+1)
+        self.assertEqual(num_elementos_pos_post, respuesta_pre_post + 1)
 
     def test_post_list(self):
         """
@@ -101,10 +105,44 @@ class TestMusicaView(APITestCase):
                          'cancion': 'cancion que deberia estar incluida en mi lista', 
                          'artista':'artista test', 'año':'2021-06-16', 'genero':'Metal'
                          }
-        respuesta_post = self.client.post(self.response, data_info)   
-        respuesta_get  = self.client.get(self.response)
-        comprobacion_post = Musica.objects.filter(cancion__contains='cancion que deberia estar incluida en mi lista')
-        if comprobacion_post.exists():
-            print(comprobacion_post)
-            print ("El registro nuevo aparece en la lista")
-        self.assertEqual(respuesta_get.data[2], respuesta_post.data)
+        respuesta_post = self.client.post(self.response, data_info)  
+        # print(respuesta_post.data['id']) 
+        valor_consulta = Musica.objects.get(id=respuesta_post.data['id'])
+        self.assertEqual(data_info['cancion'], valor_consulta.cancion)
+        self.assertEqual(data_info['artista'], valor_consulta.artista)
+        self.assertEqual(data_info['año'], str(valor_consulta.año))
+    
+    def test_PUT_success(self):
+        """
+        Prueba si el usuario loggeado puede actualizar TOTALMENTE
+        un registro.
+        """
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        actualizacion = reverse('musica:detail', kwargs={'pk' : self.musica1.id})
+        data_info= {
+                    'cancion': 'cancion que deberia estar incluida en mi lista', 
+                    'artista':'artista test', 'año':'2021-06-16', 'genero':'Metal'
+                    }
+        respuesta_actualizada = self.client.put(actualizacion, data_info)
+        print(respuesta_actualizada.data)
+        self.assertEqual(200, respuesta_actualizada.status_code)
+        valor_consulta = Musica.objects.get(id=respuesta_actualizada.data['id'])
+        #print(valor_consulta)
+        self.assertEqual(respuesta_actualizada.data['cancion'], valor_consulta.cancion)
+        self.assertEqual(respuesta_actualizada.data['artista'], valor_consulta.artista)
+        self.assertEqual(respuesta_actualizada.data['año'], str(valor_consulta.año))
+
+    def test_DELETE(self):
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        url_delete = reverse('musica:detail', kwargs={'pk' : self.musica1.id})
+        num_regitros_pre_delete = Musica.objects.all().count()
+        respuesta_delete = self.client.delete(url_delete)
+        print(respuesta_delete.data)
+        num_regitros_actual = Musica.objects.all().count()
+        self.assertEqual(204, respuesta_delete.status_code)
+        self.assertEqual(num_regitros_pre_delete - 1, num_regitros_actual)
+
+        recuperar_registro = Musica.objects.filter(pk=self.musica1.id).exists()
+        self.assertFalse(recuperar_registro)
+        print(recuperar_registro)
+
