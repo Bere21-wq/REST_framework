a30c4af9470db4d0387ac09cc5989a418c33b1b4
Barbara Perez <bere@getnea.com>
2021-07-08 11:16:49 -0500

    Se agregan allowed host

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = "django-insecure-38ur#6mfc9q(rx7lem-)r7=-3#y(&yw%#3y@154epn9d+0!(=%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
